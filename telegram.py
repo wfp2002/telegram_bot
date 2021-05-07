627384b011ddd1a112797d370e0e5584882c1e43
Wagner Fabiano <wfp2002@msn.com>
2021-05-07 09:04:55 -0300

    Add files via upload

---
+++ telegram.py
@@ -0,0 +1,38 @@
+import requests, re, time
+
+targetAlta = 60.0
+targetBaixa = 40.0
+
+def sendMessage(botMessage):
+    botToken = '1759413148:AAGZJRnU5qxu2ZPyOc17bs9sRyNYX6TYm3I'
+    botChatID = '177744171'
+    sendText = 'https://api.telegram.org/bot' + botToken + '/sendMessage?chat_id=' + botChatID + '&text=' + botMessage 
+
+    response = requests.get(sendText)
+    return response.json()
+
+while True:
+    try:
+        treeweb = requests.get("https://bscscan.com/token/0xf0fcd737fce18f95621cc7841ebe0ea6efccf77e")
+        seedweb = requests.get("https://bscscan.com/token/0x40B34cC972908060D6d527276e17c105d224559d")
+        p = re.compile("[$][0-9]{1,4}\.[0-9]{1,4}")
+        result1 = p.search(treeweb.text)
+        result2 = p.search(seedweb.text)
+
+        tree = str(result1.group(0).replace('$',''))
+        seed = str(result2.group(0).replace('$',''))
+
+        result = tree + "," + seed
+        print(result)
+
+        if (float(seed) > targetAlta):
+            print('Maior que $ ' + str(targetAlta))
+            sendMessage('SEED em Alta: $' + str(seed))
+        elif (float(seed) < targetBaixa):
+            print('Menor que $ ' + str(targetBaixa))
+            sendMessage('SEED em Baixa: $' + str(seed))
+
+        time.sleep(1200)
+
+    except Exception:
+        pass
